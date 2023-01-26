1f38fa054cc19eda0b1f40fa9a26144706ea98b6
red-fsf-ai <to_v1@hotmail.com>
2023-01-26 16:20:59 -0500

    whoops

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,6 @@ $(document).ready(function(){
     var activeNote = null;
     // colour
     // save
-        // on click
-        // capture the value of the title
-        // capture the value of the note body
-        // create a new date object
-        // create a new element that contains the title, note, and date of the note and append it to a note list
-
-
-        // validate that the note isn't empty before saving
-        // keep track of what note you're working with
     // delete
     // display
 });

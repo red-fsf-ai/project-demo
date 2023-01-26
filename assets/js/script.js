f9bf79da6f15960466ec64d5c78af82b27cb1e4d
blue-fsf-ai <veronicato6@hotmail.com>
2023-01-26 16:27:59 -0500

    yep

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,53 @@ $(document).ready(function(){
     var noteCount = 0;
     var activeNote = null;
     // colour
-    // save
+    // save button
     $('#btn-save').click(function(){
+        // captures note form values
         var title = $('#title-field').val();
         var body = $('#body-field').val();
+        // validation
         if (title === '' && body === '') {
-            alert ('Please add a title or body to your note.');
-            return;
+          alert ('Please add a title or body to your note.');
+          return;
         }
+        // new date object
         var created = new Date();
+        // retrieves notepad's colour
+        var color = $('#notepad').css('background-color');
+        // sets the note's id
         var id = noteCount + 1;
-        console.log($('<div id="note' + id + '"><div class="list-title">' + title + '</div> <div class="list-date">' + created.toLocaleString("en-US") + '</div> <div class="list-text">' + body + '</div> </div>'));
-        // $('').append('<div id="note' + id + '"><div class="list-title">' + title + '</div> <div class="list-date">' + created.toLocaleString("en-US") + '</div> <div class="list-text">' + body + '</div> </div>');
-        })
+        // activeNote tracks whether the note is new or not
+        // a note is new if activeNote is null (aka the note does not have an id yet)
+        // if the note has an id, activeNote is true
+        if (activeNote) {
+          // retrieves and updates the child elements of a specific saved note by referencing its id number which is saved as an attribute
+          $('#' + activeNote)[0].children[0].innerHTML = title;
+          $('#' + activeNote)[0].children[1].innerHTML = created.toLocaleString("en-US");
+          $('#' + activeNote)[0].children[2].innerHTML = body;
+          $('#' + activeNote)[0].style.backgroundColor = color;
+          // sets active note back to null, because the note has been saved
+          activeNote = null;
+          // hides the "edit mode" subtitle in the headings
+          $('#edit-mode').removeClass('display').addClass('no-display');
+        } else {
+          // if active note is null (aka the note does not have an id yet)
+          // create a new date object
+          var created = new Date();
+          // create a new element and append to the listed div
+          // the element is a div with the id number, inline styling with background title
+          // the div contains three nested divs each containing the title, date converted to US time formatting, and note body, respectively
+          $('#listed').append('<div id="note' + id + '" style="background-color: ' + color + '"><div class="list-title">' + title + '</div> <div class="list-date">' + created.toLocaleString("en-US") + '</div> <div class="list-text">' + body + '</div> </div>');
+          // increase the note count id
+          noteCount++;
+        };
+        // reset the note form in both cases
+        $('#title-field').val('');
+        $('#body-field').val('');
+        $('#notepad').css('background-color', 'white');
+        $('#title-field').css('background-color', 'white');
+        $('#body-field').css('background-color', 'white');
+      });
     // delete
     // display
 });

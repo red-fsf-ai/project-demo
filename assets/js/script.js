484b4cb4526b2de3038634f8aeceb669a3bca623
blue-fsf-ai <veronicato6@hotmail.com>
2023-01-26 16:26:52 -0500

    wow

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,15 @@ $(document).ready(function(){
     $('#btn-save').click(function(){
         var title = $('#title-field').val();
         var body = $('#body-field').val();
+        if (title === '' && body === '') {
+            alert ('Please add a title or body to your note.');
+            return;
+        }
         var created = new Date();
         var id = noteCount + 1;
         console.log($('<div id="note' + id + '"><div class="list-title">' + title + '</div> <div class="list-date">' + created.toLocaleString("en-US") + '</div> <div class="list-text">' + body + '</div> </div>'));
-    });
+        // $('').append('<div id="note' + id + '"><div class="list-title">' + title + '</div> <div class="list-date">' + created.toLocaleString("en-US") + '</div> <div class="list-text">' + body + '</div> </div>');
+        })
     // delete
     // display
 });
